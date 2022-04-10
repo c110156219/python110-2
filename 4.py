80055ecae3c01a42a6230d975c3389b1f2114d14
unknown <C110156219@nkust.edu.tw>
2022-04-10 22:16:09 +0800

    first commit

--- 4.py
+++ 4.py
@@ -3,10 +3,14 @@ x=int(input("X軸座標:"))
 y=int(input("Y軸座標:"))
 if x==0 and y==0:
     print("該點位於原點")
-elif x==0 and y!=0:
-    print("該點位於Y軸,離原點距離為根:",x**2+y**2)
-elif x!=0 and y==0:
-    print("該點位於X軸,離原點距離為根號",x**2+y**2)
+elif x==0 and y>0:
+    print("該點位於上半平面Y軸上,離原點距離為根:",x**2+y**2)
+elif x==0 and y<0:
+    print("該點位於下半平面Y軸上,離原點距離為根:",x**2+y**2)
+elif x>0 and y==0:
+    print("該點位於右半平面X軸,離原點距離為根號",x**2+y**2)
+elif x<0 and y==0:
+    print("該點位於左半平面X軸,離原點距離為根號",x**2+y**2)
 elif x>0 and y>0:
     print("該點位於第一象限,離原點距離為根號",x**2+y**2)
 elif x<0 and y>0:
